2ea84e8480db4f1ac6e5f1186c9dfff2b908a976
Huzaifa Khan <muhammadhuzaifakhan277@gmail.com>
2022-11-14 18:03:20 +0500

    some basc things

--- MeetUp/android/app/src/main/kotlin/com/example/connecting_hearrts/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.connecting_hearrts
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- book_tracker_app/android/app/src/main/kotlin/com/example/book_tracker_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.book_tracker_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
